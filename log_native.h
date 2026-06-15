a6780bfb77c1c7d00df94dd4fa44fa7d7926c003
Inventor Dave <102000247+inventordave2@users.noreply.github.com>
2026-06-15 11:31:23 +0100

    Create log_native.h

---
+++ log_native.h
@@ -0,0 +1,11 @@
+
+#ifndef LOG_NATIVE_H
+#define LOG_NATIVE_H
+#endif
+
+struct result LOGb( double X, int Base, double epsilon );
+int EQ( double A, double B, double epsilon );
+int test_log_native_impl( double X );
+
+double Root_NewtonsMethod( double a );
+
