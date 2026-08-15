42ab700beb6226cab5a025b3dda1efdd982d936d
Inventor Dave <102000247+inventordave2@users.noreply.github.com>
2026-08-15 14:05:46 +0100

    Add speed test macros and new function declarations

--- log_native.h
+++ log_native.h
@@ -1,11 +1,27 @@
 
 #ifndef LOG_NATIVE_H
 #define LOG_NATIVE_H
-#endif
+
+#include <time.h>
+
+#define SPEEDTESTINIT() clock_t begin; clock_t end;
+
+#define SPEEDTESTBEGIN() begin = clock();
+#define SPEEDTESTEND() end = clock();
+#define CALCSPEED() timespent = (double)(end - begin) \/ CLOCKS_PER_SEC;
 
 struct result LOGb( double X, int Base, double epsilon );
 int EQ( double A, double B, double epsilon );
 int test_log_native_impl( double X );
 
 double Root_NewtonsMethod( double a );
+double quick_cosine( double angle_in_radians );
 
+
+double quickexp( double base, int radix );
+int factorial( int n );
+
+#define APNATIVERANGE unsigned long long
+#define NUMERICAL_DIGITS_ASCII_OFFSET 48
+
+#endif
